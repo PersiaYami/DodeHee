df25a12fd86f0a8dec74a3dddef0264242dedc9b
PersiaYami <76977424+PersiaYami@users.noreply.github.com>
2021-01-09 18:35:21 +0700

    php
    UPGRADE V0.8.9: 07/01/2021
    --------------
    ✅ Super WALL-HACK.
    ✅ Super Scope.
    ✅ Super Jump.
    ✅ Super Speed.
    ✅ No Fog.
    ✅ No Grass.
    ✅ Expand View.
    ✅ Go Through Wails.
    ✅ Instant bullet
    ✅ No Recoil.
    ✅Mode AQUAMAN
    ✅ Flying Car
    ⌛ Speed Car
    ⌛Teleportation with car
    ⌛Teleportation Player

--- README.php
+++ README.php
@@ -1,1 +1,18 @@
-dsfsdfdsfdsfdsfdsfdsfdsfdsf
+
+UPGRADE V0.8.9: 07/01/2021
+--------------
+✅ Super WALL-HACK.
+✅ Super Scope.
+✅ Super Jump.
+✅ Super Speed.
+✅ No Fog.
+✅ No Grass.
+✅ Expand View.
+✅ Go Through Wails.
+✅ Instant bullet
+✅ No Recoil.
+✅Mode AQUAMAN
+✅ Flying Car
+⌛ Speed Car
+⌛Teleportation with car
+⌛Teleportation Player
